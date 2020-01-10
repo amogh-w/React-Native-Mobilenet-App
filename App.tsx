d716280964aa2b719d82849a2652aaf7dc0f359f
Amogh Warkhandkar <amoghw2025@gmail.com>
2020-01-10 23:05:22 +0530

    Added Tensorflow.js

--- App.tsx
+++ App.tsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import * as tf from "@tensorflow/tfjs";
 
-export default function App() {
+const App: React.FC = () => {
+  const [isTfReady, setIsTfReady] = useState(false);
+
+  useEffect(() => {
+    (async function mango() {
+      await tf.ready();
+      setIsTfReady(true);
+    })();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Hello!</Text>
+      <Text>TF Status: {isTfReady ? "👌" : "⏳"}</Text>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -17,3 +28,5 @@ const styles = StyleSheet.create({
     justifyContent: "center"
   }
 });
+
+export default App;
