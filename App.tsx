9bd5346bc201b959792714b46f72b43ace0b2818
Amogh Warkhandkar <amoghw2025@gmail.com>
2020-01-10 23:25:11 +0530

    Added Image Fetching from External Source

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image, TextInput, Button } from "react-native";
 import * as tf from "@tensorflow/tfjs";
 import "@tensorflow/tfjs-react-native";
 import * as mobilenet from "@tensorflow-models/mobilenet";
@@ -7,6 +7,9 @@ import * as mobilenet from "@tensorflow-models/mobilenet";
 const App: React.FC = () => {
   const [isTfReady, setIsTfReady] = useState(false);
   const [mobilenetModel, setMobilenetModel] = useState(null);
+  const [image, setImage] = useState({
+    uri: "https://lawnuk.com/wp-content/uploads/2016/08/sprogs-dogs.jpg"
+  });
 
   useEffect(() => {
     (async function mango() {
@@ -26,6 +29,12 @@ const App: React.FC = () => {
       <Text>Hello!</Text>
       <Text>TF Status: {isTfReady ? "👌" : "⏳"}</Text>
       <Text>Mobilenet Model Status: {mobilenetModel ? "👌" : "⏳"}</Text>
+      <Image source={{ uri: image.uri }} style={{ width: 200, height: 200 }} />
+      <TextInput
+        style={{ width: 200, height: 40, borderColor: "gray", borderWidth: 1 }}
+        onChangeText={text => setImage({ uri: text })}
+        value={image.uri}
+      />
     </View>
   );
 };
